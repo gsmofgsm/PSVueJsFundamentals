684fcfb20587c0a6af155248a080ac9cd5cd99bc
Qing Ma <gsmofgsm@hotmail.com>
2020-11-22 00:03:33 +0100

    passing route params as props so the component can be used as a child component

--- src/parts/PartInfo.vue
+++ src/parts/PartInfo.vue
@@ -10,11 +10,20 @@ import parts from "../data/parts";
 
 export default {
   name: "PartInfo",
+  props: {
+    partType: { type: String },
+    id: {
+      type: [Number, String],
+      validator(value) {
+        return Number.isInteger(Number(value));
+      },
+    },
+  },
   computed: {
     part() {
       //   const partType = this.$route.params.partType;
       //   const id = this.$route.params.id;
-      const { partType, id } = this.$route.params;
+      const { partType, id } = this;
       return parts[partType].find((part) => part.id === +id);
     },
   },
