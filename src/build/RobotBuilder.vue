3a1caa072b80d9a580ad3c77b8b8fb64780b1a55
Qing Ma <gsmofgsm@hotmail.com>
2020-11-21 11:21:42 +0100

    style

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,7 +2,7 @@
   <div class="content">
     <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
     <div class="top-row">
-      <div class="top part">
+      <div class="top part" :style="{ border: '3px solid red' }">
         <div class="robot-name">
           {{ selectedRobot.head.title }}
           <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
