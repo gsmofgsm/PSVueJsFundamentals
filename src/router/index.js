a6662dc6b76d078194a78ebbe07e198129e9f4f6
Qing Ma <gsmofgsm@hotmail.com>
2020-11-21 23:52:13 +0100

    navigate from code

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 
 import HomePage from "../home/HomePage";
 import RobotBuilder from "../build/RobotBuilder";
+import PartInfo from "../parts/PartInfo";
 
 Vue.use(Router);
 
@@ -10,5 +11,6 @@ export default new Router({
   routes: [
     { path: "/", name: "Home", component: HomePage },
     { path: "/build", name: "Build", component: RobotBuilder },
+    { path: "/parts", name: "Parts", component: PartInfo },
   ],
 });
