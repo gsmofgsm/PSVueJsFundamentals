bbfb8780c528dfb6ef4e450b222f95b3b12c2ac5
Qing Ma <gsmofgsm@hotmail.com>
2020-11-22 00:14:49 +0100

    nested routes with children

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,11 @@ import Router from "vue-router";
 import HomePage from "../home/HomePage";
 import RobotBuilder from "../build/RobotBuilder";
 import PartInfo from "../parts/PartInfo";
+import BrowseParts from "../parts/BrowseParts";
+import RobotHeads from "../parts/RobotHeads";
+import RobotArms from "../parts/RobotArms";
+import RobotTorsos from "../parts/RobotTorsos";
+import RobotBases from "../parts/RobotBases";
 
 Vue.use(Router);
 
@@ -11,6 +16,17 @@ export default new Router({
   routes: [
     { path: "/", name: "Home", component: HomePage },
     { path: "/build", name: "Build", component: RobotBuilder },
+    {
+      path: "/parts/browse",
+      name: "BrowseParts",
+      component: BrowseParts,
+      children: [
+        { name: "BrowseHeads", path: "heads", component: RobotHeads },
+        { name: "BrowseArms", path: "arms", component: RobotArms },
+        { name: "BrowseTorsos", path: "torsos", component: RobotTorsos },
+        { name: "BrowseBases", path: "bases", component: RobotBases },
+      ],
+    },
     {
       path: "/parts/:partType/:id",
       name: "Parts",
