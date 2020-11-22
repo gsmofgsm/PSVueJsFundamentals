b0a1dc2021efccfafd440e349207a585db776841
Qing Ma <gsmofgsm@hotmail.com>
2020-11-22 14:47:35 +0100

    bind and update lifesycle hooks of directives

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="part" :class="position">
+    {{ pinPadding }}
     <img @click="showPartInfo" :src="selectedPart.src" title="arm" />
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <span
-      v-pin="{ bottom: '10px', right: '5px' }"
+      @click="pinPadding = '30px'"
+      v-pin="{ bottom: pinPadding, right: pinPadding }"
       class="sale"
       v-show="selectedPart.onSale"
       >Sale!</span
@@ -41,7 +43,7 @@ export default {
     },
   },
   data() {
-    return { selectedPartIndex: 0 };
+    return { selectedPartIndex: 0, pinPadding: "10px" };
   },
   computed: {
     selectedPart() {
--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,15 +1,29 @@
-export default {
-  bind: (element, binding) => {
-    // console.log("arg:", binding.arg, "modifiers:", binding.modifiers);
-    // if (binding.arg !== "position") return;
-    // Object.keys(binding.modifiers).forEach((key) => {
-    //   element.style[key] = "5px";
-    // });
-    Object.keys(binding.value).forEach(position => {
-      element.style[position] = binding.value[position];
-    });
-    element.style.position = "absolute";
-    // element.style.bottom = "5px";
-    // element.style.right = "5px";
-  },
-};
+// function applyStyle(element, binding) {
+//   Object.keys(binding.value).forEach((position) => {
+//     element.style[position] = binding.value[position];
+//   });
+//   element.style.position = "absolute";
+// }
+
+// export default {
+//   bind: (element, binding) => {
+//     // console.log("arg:", binding.arg, "modifiers:", binding.modifiers);
+//     // if (binding.arg !== "position") return;
+//     // Object.keys(binding.modifiers).forEach((key) => {
+//     //   element.style[key] = "5px";
+//     // });
+//     applyStyle(element, binding);
+//     // element.style.bottom = "5px";
+//     // element.style.right = "5px";
+//   },
+//   update: (element, binding) => {
+//     applyStyle(element, binding);
+//   },
+// };
+
+export default function(element, binding) {
+  Object.keys(binding.value).forEach((position) => {
+    element.style[position] = binding.value[position];
+  });
+  element.style.position = "absolute";
+} // this bind to both the bind and update lifesycle hooks
