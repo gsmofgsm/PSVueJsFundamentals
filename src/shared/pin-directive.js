74b366daa36e37c27790a42f009d4d35333f9cf0
Qing Ma <gsmofgsm@hotmail.com>
2020-11-22 14:38:23 +0100

    pass data to directives using value notition

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,9 +1,12 @@
 export default {
   bind: (element, binding) => {
-    console.log("arg:", binding.arg, "modifiers:", binding.modifiers);
-    if (binding.arg !== "position") return;
-    Object.keys(binding.modifiers).forEach((key) => {
-      element.style[key] = "5px";
+    // console.log("arg:", binding.arg, "modifiers:", binding.modifiers);
+    // if (binding.arg !== "position") return;
+    // Object.keys(binding.modifiers).forEach((key) => {
+    //   element.style[key] = "5px";
+    // });
+    Object.keys(binding.value).forEach(position => {
+      element.style[position] = binding.value[position];
     });
     element.style.position = "absolute";
     // element.style.bottom = "5px";
