f992df5b27f5559a565ad31bc56f5f5ad18ccd95
Qing Ma <gsmofgsm@hotmail.com>
2020-11-22 12:24:42 +0100

    dispatch actions to get data from api

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content">
+  <div v-if="availableParts" class="content">
     <div class="preview">
       <CollapsibleSection>
         <div class="preview-content">
@@ -57,13 +57,15 @@
 </template>
 
 <script>
-import availableParts from "../data/parts";
 import createdHookMixin from "./created-hook-mixin";
 import PartSelector from "./PartSelector";
 import CollapsibleSection from "../shared/CollapsibleSection";
 
 export default {
   name: "RobotBuilder",
+  created() {
+    this.$store.dispatch("getParts"); // use commit to commit mutations and use dispatch to dispatch actions
+  },
   beforeRouteLeave(to, from, next) {
     if (this.addedToCart) {
       next(true);
@@ -77,7 +79,6 @@ export default {
   components: { PartSelector, CollapsibleSection },
   data() {
     return {
-      availableParts,
       cart: [], // initialization in data() function is needed for vue to notice its change elsewhere
       selectedRobot: {
         head: {},
@@ -90,6 +91,9 @@ export default {
   },
   mixins: [createdHookMixin],
   computed: {
+    availableParts() {
+      return this.$store.state.parts;
+    },
     headBorderStyle() {
       return {
         border: this.selectedRobot.head.onSale
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,29 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     cart: [], // default is import for vue to notice later changes
+    parts: null,
   },
   mutations: {
     addRobotToCart(state, robot) {
       state.cart.push(robot);
+    },
+    updateParts(state, parts) {
+      state.parts = parts;
+    },
+  },
+  actions: {
+    getParts({ commit }) {
+      // {state, getters, commit, dispatch} out of context
+      axios
+        .get("/api/parts") // get parts from api, whose target is configured in vue.config.js by proxy
+        .then((result) => commit("updateParts", result.data)) // then commit mutations to update store
+        .catch(console.error);
     },
   },
   getters: {
